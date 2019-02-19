cce900e683b03037781357b07870f9d9d99a8759
lkwowfs163com <lkwowfs@163.com>
2019-02-19 09:31:09 +0800

    使用ribbon客户端负载均衡，作为消费者调用生产者工具

--- src/main/java/com/template/ie/SpringcloudMovieApplication.java
+++ src/main/java/com/template/ie/SpringcloudMovieApplication.java
@@ -2,6 +2,7 @@ package com.template.ie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class SpringcloudMovieApplication {
 
 	@Bean
+	@LoadBalanced	// 添加ribbon，具备负载均衡能力
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- src/main/java/com/template/ie/movie/MovieController.java
+++ src/main/java/com/template/ie/movie/MovieController.java
@@ -1,6 +1,10 @@
 package com.template.ie.movie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +16,28 @@ import com.template.ie.movie.model.User;
 @RestController
 public class MovieController {
 
+	private Logger logger = LoggerFactory.getLogger(MovieController.class); 
+	
 	@Autowired
 	private RestTemplate restTemplate;
+	@Autowired
+	private LoadBalancerClient loadBalancedClient;
 	
+	/**
+	 * restTemplate - 调用测试
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public User queryUserById(@PathVariable Long id) {
 		return this.restTemplate.getForObject("http://localhost:8080/" + id, User.class);
+	}
+	
+	/**
+	 * 客户端负载均衡 - 调用测试
+	 */
+	@RequestMapping(value = "/log-instance", method = RequestMethod.GET)
+	public void logUserInstance(@PathVariable Long id) {
+		// 
+		ServiceInstance serviceInstance = this.loadBalancedClient.choose("user");
+		logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
 	}
 }
